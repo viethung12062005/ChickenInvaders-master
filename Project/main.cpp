ad979af017dafa33591f611143261cf705ac6b76
viethung12062005 <viethung12062005@gmail.com>
2024-05-07 21:20:40 +0700

    first commit

---
+++ Project/main.cpp
@@ -0,0 +1,14 @@
+#include "game_management.h"
+
+int main(int argc,char* argv[]){
+    game_management game;
+    game.init("YASUO");
+    game.menu("Start");
+    while (game.get_running())
+    {
+        game.handle_event();
+        game.handle_game();
+    }
+    game.clean();
+    return 0;
+}
